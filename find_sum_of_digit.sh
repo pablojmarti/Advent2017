5b8fa32c954187023b14defd35e5408abfbb8c0e
Pablo Marti <pablojmarti@gmail.com>
2017-12-02 09:49:25 -0500

    Squashed commit of the following:
         Adds second part of the answer to the repo

--- find_sum_of_digit.sh
+++ find_sum_of_digit.sh
@@ -1,26 +1,44 @@
 #!/usr/bin/env bash
 
-numbers=$( cat puzzle_input.txt )
-found_numbers=""
-total_sum=0
-SIZE=$(( ${#numbers}-1 ))
+# variable declaration
+numbers=$( cat puzzle_input.txt )     # pull number list from file
+found_numbers=""                      # found numbers from list
+total_sum=0                           # total sum
+SIZE=$(( ${#numbers} ))               # size of array
+LAST_INDEX=$(( ${#numbers}-1 ))
+half_size=$(( $SIZE/2 ))              # half the size of the array
 
+# loop through the array of numbers
 for (( i=0; i<${#numbers}; i++ )); do
-  next=$(( $i+1 ))
- if [[ "${numbers:$i:1}" == "${numbers:$next:1}" ]];
- then
-   found_numbers="$found_numbers${numbers:$i:1}"
- fi
+  # get the index of an item half the size of the array forward
+  half_away=$(( $i+$half_size ))
+
+  # get the index of an item half the size of the array back since the
+  # list is circular
+  half_back=$(( $half_away-$SIZE ))
+
+  # if half away is less than or equal to size
+  if [[ $half_away -le $SIZE ]];
+  then
+
+    # if the number @ the current index matches 
+    # the number half the index away
+    if [[ "${numbers:$i:1}" == "${numbers:$half_away:1}" ]];
+    then
+
+      # add current number that matches to the total sum 
+      total_sum=$(( $total_sum+${numbers:$i:1} ))
+    fi
+
+    # else half_away is greater than or equal to size
+  else
+    if [[ "${numbers:$i:1}" == "${numbers:$half_back:1}" ]];
+    then
+      # add current number that matches to the total sum
+      total_sum=$(( $total_sum+${numbers:$i:1} ))
+    fi
+  fi
 done
 
-for (( i=0; i<${#found_numbers}; i++ )); do
-  total_sum=$(( $total_sum+${found_numbers:$i:1} )) 
-done
-
-if [[ "${numbers:0:1}" == "${numbers:$SIZE:1}" ]];
-then
-  total_sum=$(( $total_sum+${numbers:$SIZE:1} ))
-fi
-
-echo $total_sum
+echo "Total Sum: $total_sum"
 
