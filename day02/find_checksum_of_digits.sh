9a4b0f91c7d1999e98fef2d0c1881a124af7b5a2
Pablo Marti <pablojmarti@gmail.com>
2017-12-02 20:49:14 -0500

    Improve day2 script to include part 2 of the question
     Squashed commit of the following:
    
         FINAL ANSWER ACHIEVED

--- day02/find_checksum_of_digits.sh
+++ day02/find_checksum_of_digits.sh
@@ -1,5 +1,6 @@
 #!/usr/bin/env bash
 
+# variables
 puzzle_file=day02_puzzle_input.txt
 ROW_COUNT=$( awk '{print NF}' $puzzle_file | sort -nu | tail -n 1 )
 COLUMN_COUNT=$( cat $puzzle_file | wc -l )
@@ -7,16 +8,39 @@ MAX=""
 MIN=""
 TOTAL_SUM=0
 
-i=1
+# while loop to read in the input
 while IFS= read -a line; do
-  MAX=$(echo ${line[*]} | tr " " "\n" | sort -nr | tr "\n" " " | cut -d " " -f1)
-  MIN=$(echo ${line[*]} | tr " " "\n" | sort -n | tr "\n" " " | cut -d " " -f1)
-  
-  ROW_VALUE=$(( $MAX-$MIN ))
 
-  echo $ROW_VALUE
+  # Outer for loop
+  for i in $line;
+  do
+
+    # inner for loop
+    for j in $line;
+    do
+      
+      # store the remainder from modulo into a var
+      remainder=$(( $i % $j ))
+
+      # if the remaind is equal to zero meaning
+      # the two numbers are divisible
+      if [[ $remainder -eq 0 ]];
+      then
+
+        #then, if the numbers are not the same
+        if [[ ! $i -eq $j ]];
+        then
+
+          # store the quotient into a var
+          ROW_VALUE=$(( $i / $j ))
+        fi
+      fi
+    done
+  done
+
+  # add the total sum to the value from the row
   TOTAL_SUM=$(( $TOTAL_SUM+$ROW_VALUE ))
-  
-  let i++
 done < "$puzzle_file"
+
+# stdout total sum
 echo $TOTAL_SUM
